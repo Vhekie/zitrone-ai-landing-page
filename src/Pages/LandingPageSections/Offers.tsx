6b672b50e79c963c9851e6744585c050dd49f0a8
Victoria <vaajibade@gmail.com>
2025-07-17 22:21:10 +0200

    create an offers section and implemented the responsitivity

--- src/Pages/LandingPageSections/Offers.tsx
+++ src/Pages/LandingPageSections/Offers.tsx
@@ -1,7 +1,52 @@
 export function Offers() {
   return (
-    <div>
-      <img src="offers.png " />
+    <div className="relative">
+      <div>
+        <img className="hidden md:inline" src="businessPeople.png " />
+        <img className=" md:hidden" src="businessPeopleLong.png " />
+      </div>
+      <div className="absolute bottom-6 md:bottom-10">
+        <div className="pl-6 md:pl-18 mb-4">
+          <div className="text-xl w-[45%] md:w-[100%] md:text-5xl font-bold mb-2">
+            Dynamic Cross-Selling & Upselling
+          </div>
+          <div className="text-thin w-[95%] md:w-[55%]">
+            Zitrone AI understands you better over time, prioritizing answers
+            and sharing solutions by Guide you effortlessly through complex
+            product catalogs.​
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:px-18 px-6">
+          <div className="bg-white/30 backdrop-blur-md p-2 md:p-8 rounded-xl shadow-lg border border-white">
+            <div className="text-lg md:text-4xl">39%</div>
+            <div className="text-xs md:text-sm">
+              Increase in average order value
+            </div>
+          </div>
+
+          <div className="bg-white/30 backdrop-blur-md p-2 md:p-8 rounded-xl shadow-lg border border-white">
+            <div className="text-lg md:text-4xl">49%</div>
+            <div className="text-xs md:text-sm">
+              Reduction in cart abandonment
+            </div>
+          </div>
+
+          <div className="bg-white/30 backdrop-blur-md p-2 md:p-8 rounded-xl shadow-lg border border-white">
+            <div className="text-lg md:text-4xl">2.5x</div>
+            <div className="text-xs md:text-sm">
+              Higher repeat purchase rate
+            </div>
+          </div>
+
+          <div className="flex flex-col space-x-2 bg-white/30 backdrop-blur-md p-2 md:p-8 rounded-xl shadow-lg border border-white">
+            <div className="text-lg md:text-4xl">80%</div>
+            <div className="text-xs md:text-sm">
+              Customer queries resolved without human intervention
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
